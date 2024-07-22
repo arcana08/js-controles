b36449eec3a3c066726cc8e59aa1f0302a7bb187
Angel Baez <angi020101@gmail.com>
2024-07-22 19:55:33 -0400

    comit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",function(){
+    //console.log(document.getElementById("txt_nombre").value);
+    //console.log(document.getElementById("txt_apellido").value);
+});
+
+document.getElementById("btn_mostrar").addEventListener("click",function(){
+    alert("hola");
+});
+
+function hol(nombre,apellido){
+    console.log(nombre,apellido);
+};
